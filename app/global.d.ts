5e11d0d1d607e262808fcf003f3418be4a6da825
TUCCHI1 <tutikikin1@gmail.com>
2024-09-15 15:47:10 +0900

    refactor: update type definitions to align with @hono/react-renderer module

--- app/global.d.ts
+++ app/global.d.ts
@@ -1,15 +1,5 @@
-import {} from 'hono'
-
-type Head = {
-  title?: string
-}
-
-declare module 'hono' {
-  interface Env {
-    Variables: {}
-    Bindings: {}
-  }
-  interface ContextRenderer {
-    (content: string | Promise<string>, head?: Head): Response | Promise<Response>
+declare module "@hono/react-renderer" {
+  interface Props {
+    title?: string
   }
 }
