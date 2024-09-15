915094c726a2f3805d00e332b171c5cd73816a19
TUCCHI1 <tutikikin1@gmail.com>
2024-09-15 18:10:08 +0900

    style: add semicolons and reformat JSX template in _renderer.tsx for better readability

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,24 +1,26 @@
-import { reactRenderer } from "@hono/react-renderer"
-import { HasIslands } from "honox/server"
+import { reactRenderer } from "@hono/react-renderer";
+import { HasIslands } from "honox/server";
 
-export default reactRenderer(({children, title}) => {
-  return (
-    <html lang="ja">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0 "/>
-        {import.meta.env.PROD ? (
-          <HasIslands>
-            <script type="module" src="/static/client.js" />
-          </HasIslands>
-        ) : (
-          <script type="module" src="/app/client.ts" />
-        )}
-        {title ? <title>{title}</title>: ""}
-      </head>
-      <body>
-        {children}
-      </body>
-    </html>
-  )
-})
+export default reactRenderer(({ children, title }) => {
+	return (
+		<html lang="ja">
+			<head>
+				<meta charSet="utf-8" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0 "
+				/>
+
+				{import.meta.env.PROD ? (
+					<HasIslands>
+						<script type="module" src="/static/client.js" />
+					</HasIslands>
+				) : (
+					<script type="module" src="/app/client.ts" />
+				)}
+				{title ? <title>{title}</title> : ""}
+			</head>
+			<body>{children}</body>
+		</html>
+	);
+});
