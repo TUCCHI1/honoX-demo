ae8c1c9407fd446874dc5f5875704608baa006eb
TUCCHI1 <tutikikin1@gmail.com>
2024-09-15 16:31:56 +0900

    feat: update tsconfig and Vite config for improved compatibility with React
    refactor: change jsxImportSource to 'react' in tsconfig for better integration
    
    chore: enhance Vite config to support client and SSR modes
    
    chore: add Cloudflare Pages plugin to Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,28 @@
-import honox from 'honox/vite'
-import { defineConfig } from 'vite'
+import pages from "@hono/vite-cloudflare-pages";
+import honox from "honox/vite";
+import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [honox()],
-})
+export default defineConfig(({ mode }) => {
+	if (mode === "client") {
+		return {
+			build: {
+				rollupOptions: {
+					input: ["./app/client.ts"],
+					output: {
+						entryFileNames: "static/client.js",
+						chunkFileNames: "static/assets/[name]-[hash].js",
+						assetFileNames: "static/assets/[name].[ext]",
+					},
+				},
+				emptyOutDir: true,
+			},
+		};
+	}
+
+	return {
+		ssr: {
+			external: ["react", "react-dom"],
+		},
+		plugins: [honox(), pages()],
+	};
+});
