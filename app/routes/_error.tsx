4ca61cb0ac37b5afad4307eef4f0e63556dae229
TUCCHI1 <tutikikin1@gmail.com>
2024-09-15 14:59:20 +0900

    refactor: Simplified error handler to render error message directly in JSX format

--- app/routes/_error.tsx
+++ app/routes/_error.tsx
@@ -1,12 +1,7 @@
-import type { ErrorHandler } from 'hono'
+import type { ErrorHandler } from "hono";
 
-const handler: ErrorHandler = (e, c) => {
-  if ('getResponse' in e) {
-    return e.getResponse()
-  }
-  console.error(e.message)
-  c.status(500)
-  return c.render('Internal Server Error')
+const handler: ErrorHandler = (e,c) => {
+  return c.render(<h1>Error! {e.message}</h1>)
 }
 
 export default handler
